a888c11790000daac557305afbcd85a6b542d549
Abdulrahman M. <53632818+Aoof@users.noreply.github.com>
2025-04-19 19:50:12 -0400

    Added mermaid support to md files

--- src/lib/components/report/report.svelte
+++ src/lib/components/report/report.svelte
@@ -4,6 +4,7 @@
     import { Progress } from '$lib/components/ui/progress';
     import "highlight.js/styles/atom-one-dark-reasonable.css";
     import { marked } from 'marked';
+    import mermaid from 'mermaid';
 
     import reportMarkdown from './report.md?raw';
 
@@ -11,14 +12,27 @@
 
     let isLoading = true;
 
+    let diagram_index = 0;
+
     onMount(async () => {
         isLoading = true;
         reportHtml = await marked.parse(reportMarkdown);
 
         await tick();
-        document.querySelectorAll('pre code').forEach((block : Element) => {
+        document.querySelectorAll('pre code').forEach(async (block : Element) => {
+            if (block.classList.contains('language-mermaid')) {
+                const res = await mermaid.render("mermaid-diagram-" + ++diagram_index, block.innerHTML, block);
+                block.innerHTML = res.svg;
+                block.classList.remove('language-mermaid');
+                block.classList.add('language-svg');
+                block.classList.add('flex');
+                block.classList.add('justify-center');
+                return;
+            }
             hljs.highlightElement(block as HTMLElement);
         });
+
+        mermaid.initialize({ startOnLoad: true, darkMode: true, theme: 'dark' });
 
         isLoading = false;
     }) 
